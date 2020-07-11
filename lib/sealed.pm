393b4e274539b3b882d48a4805d38361135e81c8
Joe Schaefer <joe@sunstarsys.com>
2020-07-10 22:10:58 -0400

    flow

--- lib/sealed.pm
+++ lib/sealed.pm
@@ -5,12 +5,14 @@
 # Author: Joe Schaefer <joe@sunstarsys.com>
 
 package sealed;
+
 use strict;
 use warnings;
-use B::Generate ();
-use B::Deparse ();
 
-our $VERSION = v0.9.9;
+use B::Generate ();
+use B::Deparse  ();
+
+our $VERSION    = v0.9.9;
 our $DEBUG;
 
 my %valid_attrs = (sealed => 1);
@@ -20,18 +22,23 @@ my $padop       = $p_obj->START->next->next;
 sub tweak {
   my ($op, $lexical_names, $lexicals, $op_stack) = @_;
   my $tweaked = 0;
+
   if ($op->next->name eq "padsv") {
     $op = $op->next;
     my $lex = $$lexical_names[$op->targ];
+
     if ($lex->TYPE->isa("B::HV")) {
       $tweaked++;
       my $class = $lex->TYPE->NAME;
+
       while ($op->next->name ne "entersub") {	  
+
 	if ($op->next->name eq "pushmark") {
 	  splice @_, 0, 1, $op->next;
-	  (my $t, $op) = &tweak;
+	  ($op, my $t) = &tweak;
 	  $tweaked += $t;
 	}
+
 	elsif ($op->next->name eq "method_named") {
 	  my $methop = $op->next;
 	  my $targ = $methop->targ;
@@ -41,23 +48,29 @@ sub tweak {
 	  $method_name = $$lexicals[$idx++]->[$targ] while not defined $method_name;
 	  warn __PACKAGE__, ": compiling $class->$method_name lookup.\n"
 	      if $DEBUG;
+
 	  my $method = $class->can($method_name)
 	    or die "Invalid lookup: $class->$method_name - did you forget to 'use $class' first?";
 	  $_->[$targ] = $method for @$lexicals; # bulletproof, blanket bludgeon
 
+	  # replace $methop
 	  my $rv2cv = bless $padop->new($padop->name, $padop->flags), ref $padop;
 	  $rv2cv->padix($targ);
 	  $op->next($rv2cv);
 	  $rv2cv->next($methop->next);
 	  $rv2cv->sibling($methop->sibling);
 	}
+
 	$op = $op->next;
       }
+
       $op = $op->next;
     }
+
   }
+
   push @$op_stack, $op->next;
-  return ($tweaked, $op);
+  return ($op, $tweaked);
 }
 
 sub MODIFY_CODE_ATTRIBUTES {
@@ -65,11 +78,11 @@ sub MODIFY_CODE_ATTRIBUTES {
 
   if (grep exists $valid_attrs{$_}, @attrs) {
 
-    my $cv_obj = B::svref_2object($rv);
-    my @op_stack = ($cv_obj->START);
+    my $cv_obj             = B::svref_2object($rv);
+    my @op_stack           = ($cv_obj->START);
     my ($pad_names, @pads) = $cv_obj->PADLIST->ARRAY;
-    my @lex_arr = map $_->object_2svref, @pads;
-    my @lexical_names = $pad_names->ARRAY;
+    my @lex_arr            = map $_->object_2svref, @pads;
+    my @lexical_names      = $pad_names->ARRAY;
     my %processed_op;
     my $tweaked;
 
@@ -78,8 +91,7 @@ sub MODIFY_CODE_ATTRIBUTES {
         or next;
 
       if ($op->name eq "pushmark") {
-	  my ($t, $op) = tweak($op, \@lexical_names, \@lex_arr, \@op_stack);	  
-	  $tweaked += $t;
+	  $tweaked += tweak($op, \@lexical_names, \@lex_arr, \@op_stack);	  
       }
       elsif ($op->can("pmreplroot")) {
         push @op_stack, $op->pmreplroot, $op->next;
@@ -90,14 +102,16 @@ sub MODIFY_CODE_ATTRIBUTES {
 	}
 	unshift @op_stack, $op->next;
       }
-
       else {
         unshift @op_stack, $op->next;
       }
+
     }
+
     if (defined $DEBUG and $DEBUG eq "deparse" and $tweaked) {
       warn B::Deparse->new->coderef2text($rv), "\n";
     }
+
   }
 
   return grep !$valid_attrs{$_}, @attrs;
