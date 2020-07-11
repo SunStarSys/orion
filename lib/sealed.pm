7f946a2de6d80667ee9d80223aaf79bcb14fe506
Joe Schaefer <joe@sunstarsys.com>
2020-07-11 14:57:01 -0400

    XS-still-exists

--- lib/sealed.pm
+++ lib/sealed.pm
@@ -157,7 +157,8 @@ because this op-tree walker isn't as robust as it needs to be.
 Don't use this if you are writing a reusable OO module (on CPAN, say).
 This module targets end-applications: virtual method lookups and
 duck typing are core elements of any dynamic language's OO feature
-design, and Perl is no different.
+design, and Perl is no different.  Look into XS if you want peak
+performance in reusable OO methods you wish to provide.
 
 =item See Also:
 
