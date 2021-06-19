053a009d62a405412ae19ff5715fe7e60bcea327
Joe Schaefer <joe@sunstarsys.com>
2021-06-19 12:33:20 -0400

    Update sealed.pm

--- lib/sealed.pm
+++ lib/sealed.pm
@@ -102,7 +102,7 @@ sub MODIFY_CODE_ATTRIBUTES {
       $op->dump if defined $DEBUG and $DEBUG eq 'dump';
 
       if ($op->name eq "pushmark") {
-	$tweaked += tweak($op, @lexical_names, @pads, @op_stack);
+	$tweaked += tweak $op, @lexical_names, @pads, @op_stack;
       }
       elsif ($op->can("pmreplroot")) {
         push @op_stack, $op->pmreplroot, $op->next;
