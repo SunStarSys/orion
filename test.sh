930aa9ec5dcd82cb65afc55d716e0cd4dc3c7b3d
Joe Schaefer <joe@sunstarsys.com>
2022-09-17 14:37:27 -0400

    bugfixes

--- test.sh
+++ test.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
+# USAGE: $0 [clean]
+
 set -e
 set -x
 node markdownd.js &
+if [[ "${1:-}" == clean ]]; then
+  rm -rf trunk /tmp/www
+fi
 if [[ -d trunk ]]; then
   svn cleanup trunk
   svn up trunk
+  sleep 3
 else
-  svn co https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.co
+  svn co https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk
 fi
 time perl -Ilib build_site.pl --source-base=trunk --target-base=/tmp/www
 kill %1
