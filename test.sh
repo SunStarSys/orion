654cc103740dbc0382f676310e7aacedab170809
Joe Schaefer <joe@sunstarsys.com>
2022-09-17 13:11:27 -0400

    test-script

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+node markdownd.js &
+rm -rf ./trunk /tmp/www
+svn co https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk
+time perl build_site.pl --source-base=trunk --target-base=/tmp/www
+rc=$?
+kill %1
+exit $rc
