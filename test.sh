d16767186fd000be8e1aa8b8f56b3271c05e81bd
Joe Schaefer <joe@sunstarsys.com>
2022-09-17 13:43:14 -0400

    set-x

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
 set -e
+set -x
 node markdownd.js &
 rm -rf ./trunk /tmp/www
 svn co https://vcs.sunstarsys.com/repos/svn/public/cms-sites/www.sunstarsys.com/trunk
 time perl build_site.pl --source-base=trunk --target-base=/tmp/www
-rc=$?
 kill %1
-exit $rc
