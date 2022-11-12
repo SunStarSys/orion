48f7122303450b69f3e3e6673d7b949cff950bfd
Joe Schaefer <joe@sunstarsys.com>
2022-11-12 16:54:40 -0500

    skip-archived-files

--- lib/SunStarSys/Util.pm
+++ lib/SunStarSys/Util.pm
@@ -367,10 +367,10 @@ sub seed_deps {
   my $dir = dirname($path);
   read_text_file "content$path", \ my %d;
 
-  push @{$path::dependencies{$path}}, grep $_ ne $path, grep s/^content//,
+  push @{$path::dependencies{$path}}, grep $_ ne $path, grep {read_text_file $_, \ my %data; not exists $data{headers}{archive} and s/^content//}
     map glob("content$_"), map index($_, "/") == 0  ? $_ : "$dir/$_",
     ref $d{headers}{dependencies} ? @{$d{headers}{dependencies}} : split /[;,]?\s+/, $d{headers}{dependencies}
-        if exists $d{headers}{dependencies};
+        if exists $d{headers}{dependencies} and not exists $d{headers}{archive};
 
   while ($d{content} =~ /\{%\s*include\s+"([^"]+)"\s*-?%\}/g) {
     my $src = $1;
