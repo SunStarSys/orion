59dc826a348a88354f38ae84bdb41fdcfd3e907f
Joe Schaefer <joe@sunstarsys.com>
2022-11-20 19:16:23 -0500

    gzipped

--- deps2dotcfg.pl
+++ deps2dotcfg.pl
@@ -12,32 +12,36 @@ my $yaml_deps = Load $content;
 
 my $nn = 0;
 
-my ($root) = grep s!trunk/content!!, <trunk/content/sitemap.*.en> or die "Can't find root document: $!";
+my @language = qw/English Spanish German French/;
+my @lang = qw/.en .es .de .fr/;
 
-my @dep_nodes = ($root);
-my %dot;
-while (@dep_nodes and (my $node = shift @dep_nodes)) {
-  warn $node;
-  next unless exists $yaml_deps->{$node};
-  $dot{$node} = {
-    deps => $yaml_deps->{$node},
-    id => $nn++,
-    name => "\"$node\""
-  };
-  warn for @{$dot{$node}{deps}};
-  push @dep_nodes, @{$dot{$node}{deps}};
-  delete $yaml_deps->{$node};
-}
+for my $idx (0..3) {
+  my ($root) = grep s!trunk/content!!, <trunk/content/sitemap.*$lang[$idx]> or die "Can't find root document: $!";
 
-open $fh, ">:encoding(UTF-8)", "deps.gv", or die "Can't open deps.gv: $!";
-print $fh "strict digraph Dependencies {\n";
-
-for (sort {$a->{id} <=> $b->{id}} values %dot) {
-  print $fh "$_->{name} [name=$_->{name}];\n";
-  for my $value (map $dot{$_} || $_, @{$_->{deps}}) {
-    $value = $dot{$value} = { deps => $$yaml_deps{$value}, id => $nn++, name=>"\"$value\""} unless ref $value;
-    print $fh "$_->{name} -> $value->{name};\n";
+  my @dep_nodes = ($root);
+  my %dot;
+  while (@dep_nodes and (my $node = shift @dep_nodes)) {
+    next unless exists $yaml_deps->{$node};
+    $dot{$node} = {
+      deps => $yaml_deps->{$node},
+      id => $nn++,
+      name => "\"$node\""
+    };
+    push @dep_nodes, @{$dot{$node}{deps}};
+    delete $yaml_deps->{$node};
   }
+
+  open $fh, ">:encoding(UTF-8)", "deps.gv$lang[$idx]", or die "Can't open deps.gv$lang[$idx]: $!";
+  print $fh "strict digraph \"$language[$idx] Dependencies\" {\n";
+
+  for (sort {$a->{id} <=> $b->{id}} values %dot) {
+    print $fh "$_->{name} [name=$_->{name}];\n";
+    for my $value (map $dot{$_} || $_, @{$_->{deps}}) {
+      $value = $dot{$value} = { deps => $$yaml_deps{$value}, id => $nn++, name=>"\"$value\""} unless ref $value;
+      print $fh "$_->{name} -> $value->{name};\n";
+    }
+  }
+  print $fh "}\n";
+  close $fh;
+  system "dot -Tsvgz deps.gv$lang[$idx] > deps.svg$lang[$idx].gz";
 }
-print $fh "}\n";
-close $fh;
