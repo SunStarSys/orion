fb162e8634fa0aff9bf84a93ff97c748a57e7d8c
Joseph Schaefer <joe@sunstarsys.com>
2024-04-19 21:33:26 -0400

    translate

--- translate.pl
+++ translate.pl
@@ -14,6 +14,7 @@ my ($t_base, $t_dir, $t_ext) = parse_filename $targ;
 s/^[^.]*\.// for my $s_lang = $s_ext;
 s/^[^.]*\.// for my $t_lang = $t_ext;
 
+
 read_text_file $src, \ my %s_args;
 my %t_args;
 my @keys = qw/title categories keywords status acl/;
@@ -22,20 +23,50 @@ my @keys = qw/title categories keywords status acl/;
 
 delete $t_args{headers}{acl} unless defined $t_args{headers}{acl};
 
-my (@code_blocks, @katex_strings);
+my (@html, @code_blocks, @katex_strings, @dtls, @mdlinks, @snippets, @key_prefixes);
+
+#$s_args{content} =~ s{(<[^>]+>|[&]\w+;)}{
+#  push @html, $1;
+#  "&nbsp;"
+#}ge;
 
 $s_args{content} =~ s{^(\`{3}[\w-]+\n.*?\n\`{3})$}{
   push @code_blocks, $1;
   "<!-- # -->"
 }gmse;
+
 $s_args{content} =~ s{(\${2}.*?\${2})$}{
   push @katex_strings, $1;
   "<!-- ## -->"
 }ge;
+$s_args{content} =~ s{(\{[\%\#\{].*?[\%\#\}]\})}{
+  push @dtls, $1;
+  "<!-- ### -->"
+}ge;
+$s_args{content} =~ s{](\(.*?\))}{
+  push @mdlinks, $1;
+  "]<!-- #### -->"
+}ge;
+$s_args{content} =~ s{(\[snippet:[^\]]+\])}{
+  push @snippets, $1;
+  "<!-- ##### -->"
+}ge;
+
+if ($s_ext =~ /^ya?ml\b/) {
+  $s_args{content} =~ s{^(\s*(?:- )?[\w-]+: )}{
+    push @key_prefixes, $1;
+    "<!-- ###### -->"
+  }gmse;
+}
 
 $t_args{content} = join "\n\n", translate $s_lang, $t_lang, split /\n\n/, $s_args{content};
-$t_args{content} =~ s{<!-- # -->}{shift @code_blocks}ge;
+#$t_args{content} =~ s{\Q&nbsp;}{shift @html}ge;
+$t_args{content} =~ s{<!-- ###### -->}{shift @key_prefixes}ge;
+$t_args{content} =~ s{<!-- ##### -->}{shift @snippets}ge;
+$t_args{content} =~ s{<!-- #### -->}{shift @mdlinks}ge;
+$t_args{content} =~ s{<!-- ### -->}{shift @dtls}ge;
 $t_args{content} =~ s{<!-- ## -->}{shift @katex_strings}ge;
+$t_args{content} =~ s{<!-- # -->}{shift @code_blocks}ge;
 
 if (exists $s_args{headers}{dependencies}) {
   s/\.$s_lang/.$t_lang/g, utf8::encode $_ for $t_args{headers}{dependencies} = $s_args{headers}{dependencies};
@@ -59,11 +90,19 @@ sub translate {
       text => $args[$idx-1],
     };
   }
-  local $_ = Cpanel::JSON::XS->new->utf8(1)->encode(\@obj);
+
+  my @rv;
+  my $idx;
+ LOOP:
+  my @cobj = ();
+  push @cobj, shift @obj while @obj and @cobj < 100;
+  return @rv unless @cobj;
+  warn ++$idx;
+  local $_ = Cpanel::JSON::XS->new->utf8(1)->encode(\@cobj);
   open my $fh, ">:encoding(UTF-8)", ".translate.json";
   print $fh $_;
   close $fh;
-  return map {
+  push @rv, map {
     s/\\u([0-9a-f]{4})/decode('%u' . $1)/ge;
     utf8::upgrade $_;
     s/&lt;/</g;
@@ -71,4 +110,5 @@ sub translate {
     $_
   } map $_->{"translated-text"}, sort {$a->{key} <=> $b->{key}} map @{$_->{data}->{documents}},
   Load scalar qx(oci ai language batch-language-translation --target-language-code $t_lang --documents file://.translate.json);
+  goto LOOP;
 }
