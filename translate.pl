6c49b64d62cba360e16c7034d4e865379fae9341
Joseph Schaefer <joe@sunstarsys.com>
2024-04-20 13:36:02 -0400

    translate

--- translate.pl
+++ translate.pl
@@ -23,18 +23,17 @@ my @keys = qw/title categories keywords status acl/;
 
 delete $t_args{headers}{acl} unless defined $t_args{headers}{acl};
 
-my (@html, @code_blocks, @katex_strings, @dtls, @mdlinks, @snippets, @key_prefixes);
+my (@headings, @code_blocks, @katex_strings, @dtls, @mdlinks, @snippets, @key_prefixes);
 
-#$s_args{content} =~ s{(<[^>]+>|[&]\w+;)}{
-#  push @html, $1;
-#  "&nbsp;"
-#}ge;
-
+$s_args{content} =~ s{^(#+ )}{
+  push @headings, $1;
+  "<!--  -->"
+}ge;
 $s_args{content} =~ s{^(\`{3}[\w-]+\n.*?\n\`{3})$}{
   push @code_blocks, $1;
   "<!-- # -->"
 }gmse;
-$s_args{content} =~ s{(\${2}.*?\${2})}{
+$s_args{content} =~ s{(\${2}.*?\${2}|<span class="editormd-tex">.*?</span>)}{
   push @katex_strings, $1;
   "<!-- ## -->"
 }ge;
@@ -59,19 +58,23 @@ if ($s_ext =~ /^ya?ml\b/) {
 }
 
 $t_args{content} = join "\n\n", translate $s_lang, $t_lang, split /\n\n/, $s_args{content};
-#$t_args{content} =~ s{\Q&nbsp;}{shift @html}ge;
 $t_args{content} =~ s{<!-- ###### -->}{shift @key_prefixes}ge;
 $t_args{content} =~ s{<!-- ##### -->}{shift @snippets}ge;
 $t_args{content} =~ s{<!-- #### -->}{shift @mdlinks}ge;
 $t_args{content} =~ s{<!-- ### -->}{shift @dtls}ge;
 $t_args{content} =~ s{<!-- ## -->}{shift @katex_strings}ge;
 $t_args{content} =~ s{<!-- # -->}{shift @code_blocks}ge;
+$t_args{content} =~ s{<!--  -->}{shift @headings}ge;
+
+## oci rendering fixups
+$t_args{content} =~ s/"([^"]+)"\(/[$1](/g;
 
 if (exists $s_args{headers}{dependencies}) {
   s/\.$s_lang/.$t_lang/g, utf8::encode $_ for $t_args{headers}{dependencies} = $s_args{headers}{dependencies};
 }
 
 open my $fh, ">:utf8", $targ or die "open '$targ' failed: $!";
+utf8::encode $_ for map ref($_) eq "HASH" ? values %$_ : ref($_) eq "ARRAY" ?@$_ : $_, values $t_args{headers};
 my $headers = Dump $t_args{headers};
 utf8::decode $headers;
 print $fh "$headers---\n\n";
