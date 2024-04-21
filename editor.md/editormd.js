3772c6b06fb02f4320264882fc6adf861be9580b
Joseph Schaefer <joe@sunstarsys.com>
2024-04-21 12:09:42 -0400

    latest

--- editor.md/editormd.js
+++ editor.md/editormd.js
@@ -1610,9 +1610,8 @@
 
             this.previewContainer.find("." + editormd.classNames.tex).each(function(){
                 var tex  = $(this);
-                editormd.$katex.render(tex.text(), tex[0], {display: true, macros: macros_physics, throwOnError: false});
-
-                tex.find(".katex").css("font-size", "1.6em");
+                var mode = (tex.prop('nodeName') !== "SPAN");
+                editormd.$katex.render(tex.text(), tex[0], {displayMode: mode, trust: true, strict: false, macros: macros_physics, throwOnError: false});
             });
 
             return this;
@@ -3544,7 +3543,7 @@
             sequenceDiagram      : false,          // sequenceDiagram.js only support IE9+
         };
 
-        var settings        = $.extend(defaults, options || {});
+        var settings        = $.extend(true, defaults, options || {});
         //var marked          = editormd.$marked;
         var markedRenderer  = new marked.Renderer();
         markdownToC         = markdownToC || [];
@@ -3755,22 +3754,25 @@
             }
 
             text = trim(text);
+            var escapedText = text.toLowerCase().replace(/[^\w]+/g, "-");
+            var isChinese = /^[\u4e00-\u9fa5]+$/.test(text);
+            var id        = (isChinese) ? escape(text).replace(/\%/g, "") : text.toLowerCase().replace(/[^\w]+/g, "-");
 
-            var escapedText    = text.toLowerCase().replace(/[^\w]+/g, "-");
+            text = text.replace(/\$\$(.*?)\$\$/g, ($full, $1) => {return "<span class=\"" + editormd.classNames.tex +"\">"+$1+"</span>"});
+            linkText = linkText.replace(/\$\$(.*?)\$\$/g, ($full, $1) => {return "<span class=\"" + editormd.classNames.tex +"\">"+$1+"</span>"});
+
             var toc = {
                 text  : text,
                 level : level,
                 slug  : escapedText
             };
 
-            var isChinese = /^[\u4e00-\u9fa5]+$/.test(text);
-            var id        = (isChinese) ? escape(text).replace(/\%/g, "") : text.toLowerCase().replace(/[^\w]+/g, "-");
 
             markdownToC.push(toc);
 
             var headingHTML = "<h" + level + " id=\"h"+ level + "-" + this.options.headerPrefix + id +"\">";
 
-            headingHTML    += "<a name=\"" + text + "\" class=\"reference-link\"></a>";
+            headingHTML    += "<a name=\"" + escapedText + "\" class=\"reference-link\"></a>";
             headingHTML    += "<span class=\"header-link octicon octicon-link\"></span>";
             headingHTML    += (hasLinkReg) ? this.atLink(this.emoji(linkText)) : this.atLink(this.emoji(text));
             headingHTML    += "</h" + level + ">";
@@ -3788,16 +3790,16 @@
         };
 
         markedRenderer.paragraph = function(text) {
-            var isTeXInline     = /\$\$(.*)\$\$/g.test(text);
-            var isTeXLine       = /^\$\$(.*)\$\$$/.test(text);
+            var isTeXInline     = /\$\$(.*?)\$\$/g.test(text);
+            var isTeXLine       = /^\$\$(.*?)\$\$$/.test(text);
             var isTeXAddClass   = (isTeXLine)     ? " class=\"" + editormd.classNames.tex + "\"" : "";
             var isToC           = (settings.tocm) ? /^(\[TOC\]|\[TOCM\])$/.test(text) : /^\[TOC\]$/.test(text);
             var isToCMenu       = /^\[TOCM\]$/.test(text);
 
             if (!isTeXLine && isTeXInline)
             {
-                text = text.replace(/(\$\$([^\$]*)\$\$)+/g, function($1, $2) {
-                    return "<span class=\"" + editormd.classNames.tex + "\">" + $2.replace(/\$/g, "") + "</span>";
+                text = text.replace(/\$\$(.*?)\$\$/g, function($1, $2) {
+                    return "<span class=\"" + editormd.classNames.tex + "\">" + $2 + "</span>";
                 });
             }
             else
@@ -3848,10 +3850,14 @@
         };
 
         markedRenderer.listitem = function(text) {
+            text = text.replace(/\$\$(.*?)\$\$/g, ($full, $1) => {
+                        return "<span class=\"" + editormd.classNames.tex + "\">" + $1 + "</span>";
+                    });
             if (settings.taskList && /^\s*\[[x\s]\]\s*/.test(text))
             {
                 text = text.replace(/^\s*\[\s\]\s*/, "<input type=\"checkbox\" class=\"task-list-item-checkbox\" /> ")
-                           .replace(/^\s*\[x\]\s*/,  "<input type=\"checkbox\" class=\"task-list-item-checkbox\" checked disabled /> ");
+                    .replace(/^\s*\[x\]\s*/,  "<input type=\"checkbox\" class=\"task-list-item-checkbox\" checked disabled /> ")
+                ;
 
                 return "<li style=\"list-style: none;\">" + this.atLink(this.emoji(text)) + "</li>";
             }
@@ -3887,6 +3893,7 @@
         {
             var text  = toc[i].text;
             var level = toc[i].level;
+            var slug  = toc[i].slug;
 
             if (level < startLevel) {
                 continue;
@@ -3905,7 +3912,7 @@
                 html += "</ul></li>";
             }
 
-            html += "<li><a class=\"toc-level-" + level + "\" href=\"#" + text + "\" level=\"" + level + "\">" + text + "</a><ul>";
+            html += "<li><a class=\"toc-level-" + level + "\" href=\"#" + slug + "\" level=\"" + level + "\">" + text + "</a><ul>";
             lastLevel = level;
         }
 
@@ -4094,7 +4101,7 @@
         var defaults = {
             gfm                  : true,
             toc                  : true,
-            tocm                 : false,
+            tocm                 : true,
             tocStartLevel        : 1,
             tocTitle             : "目录",
             tocDropdown          : false,
@@ -4102,13 +4109,13 @@
             markdown             : "",
             markdownSourceCode   : false,
             htmlDecode           : false,
-            autoLoadKaTeX        : true,
+            autoLoadKaTeX        : false,
             pageBreak            : true,
             atLink               : true,    // for @link
             emailLink            : true,    // for mail address auto link
-            tex                  : false,
-            taskList             : false,   // Github Flavored Markdown task lists
-            emoji                : false,
+            tex                  : true,
+            taskList             : true,   // Github Flavored Markdown task lists
+            emoji                : true,
             flowChart            : false,
             sequenceDiagram      : false,
             previewCodeHighlight : true
@@ -4204,7 +4211,7 @@
             if (settings.mermaid && editormd.$mermaid) {
                 var idx = 0;
                 for (const e of previewContainer.find(".mermaid").toArray()) {
-                    editormd.$mermaid.render("mermaid-" + ++idx, $(e).text(), function(graph) {
+                    mermaid.render("mermaid-" + ++idx, $(e).text(), function(graph) {
                         e.outerHTML = graph;
                     });
                 }
@@ -4223,8 +4230,8 @@
             var katexHandle = function() {
                 div.find("." + editormd.classNames.tex).each(function(){
                     var tex  = $(this);
-                    katex.render(tex.text(), tex[0], {display:true, macros:macros_physics, throwOnError: false});
-                    tex.find(".katex").css("font-size", "1.6em");
+                    var mode = (tex.prop('nodeName') !== "SPAN");
+                    editormd.$katex.render(tex.text(), tex[0], {displayMode: mode, trust: true, strict: false, macros: macros_physics, throwOnError: false});
                 });
             };
 
@@ -4238,6 +4245,7 @@
             }
             else
             {
+                editormd.$katex = katex;
                 katexHandle();
             }
         }
@@ -4389,7 +4397,9 @@
     editormd.katexURL  = {
         css : "/editor.md/lib/katex.min",
         js  : "/editor.md/lib/katex.min",
-        physics: "/editor.md/lib/katex-physics"
+        physics: "/editor.md/lib/katex-physics",
+        mhchem: "/editor.md/lib/mhchem",
+        copy: "/editor.md/lib/copy-tex"
     };
 
     editormd.kaTeXLoaded = false;
@@ -4404,7 +4414,11 @@
     editormd.loadKaTeX = function (callback) {
         editormd.loadCSS(editormd.katexURL.css, function() {
             editormd.loadScript(editormd.katexURL.js, function() {
-                editormd.loadScript(editormd.katexURL.physics, callback || function(){
+                editormd.loadScript(editormd.katexURL.physics, function(){
+                    editormd.loadScript(editormd.katexURL.mhchem, function () {
+                        editormd.loadScript(editormd.katexURL.copy, callback || function () {
+                        });
+                    });
                 });
             });
         });
