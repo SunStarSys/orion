441e7f5c6dbb73b00ceb30954fb7ed6b516e1c9a
Joe Schaefer <joe@sunstarsys.com>
2026-05-03 19:12:34 -0400

    watch

--- watch.sh
+++ watch.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 cd /src/trunk/content
-while true; do
-  inotifywait -m -e modify,create,delete $(find . -type d) | while read path action dir; do
-    perl /src/build_site.pl --source-base /src/trunk --target-base /src/www --dirqueue content/$path --revision 0
-  done
+inotifywait -r -m -e modify,create,delete . | while read path action file; do
+  perl /src/build_site.pl --source-base /src/trunk --target-base /src/www --dirqueue content/$path --revision 0
 done
